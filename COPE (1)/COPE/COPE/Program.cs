be69eac648860b393db0452b12272c298537e9b4
HermaMG4 <mendezgutierrezcley@gmail.com>
2024-11-25 21:12:36 -0600

    Add files via upload

---
+++ COPE (1)/COPE/COPE/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        try
+        {
+            var filename = @"C:\Users\alexa\source\repos\COPE\COPE\Laptops.xml";
+
+            XElement storeXML = XElement.Load(filename);
+
+            var Laptops = from lector in storeXML.Element("Laptops").Elements("Laptop")
+                          select lector;
+
+            foreach (var leer in Laptops)
+            {
+                Console.WriteLine(leer.Value);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
